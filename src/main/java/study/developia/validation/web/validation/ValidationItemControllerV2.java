b30a33c543883577acffba7fa4b3447f59da9b90
developiaa <developiaa@gmail.com>
2021-11-06 21:43:04 +0900

    feat : Add type error message

--- src/main/java/study/developia/validation/web/validation/ValidationItemControllerV2.java
+++ src/main/java/study/developia/validation/web/validation/ValidationItemControllerV2.java
@@ -8,6 +8,7 @@ import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
+import org.springframework.validation.ValidationUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import study.developia.validation.domain.item.Item;
@@ -150,10 +151,16 @@ public class ValidationItemControllerV2 {
     @PostMapping("/add")
     public String addItemV4(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
 
+        if(bindingResult.hasErrors()){
+            log.info("errors = {} ", bindingResult);
+            return "validation/v2/addForm";
+        }
+
         log.info("objectName={}", bindingResult.getObjectName());
         log.info("target={}", bindingResult.getTarget());
 
         //검증 로직
+//        ValidationUtils.rejectIfEmptyOrWhitespace(bindingResult, "itemName", "required");
         if (!StringUtils.hasText(item.getItemName())) {
             bindingResult.rejectValue("itemName", "required");
         }
