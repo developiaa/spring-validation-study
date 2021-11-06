a65504c0cc8f9ee313363231b14c41754bb72d8e
developiaa <developiaa@gmail.com>
2021-11-06 22:10:34 +0900

    feat : Refactor validator #2 - add databinder

--- src/main/java/study/developia/validation/ValidationApplication.java
+++ src/main/java/study/developia/validation/ValidationApplication.java
@@ -2,12 +2,20 @@ package study.developia.validation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.validation.Validator;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import study.developia.validation.web.validation.ItemValidator;
 
 @SpringBootApplication
-public class ValidationApplication {
+public class ValidationApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(ValidationApplication.class, args);
     }
+
+    //global validator
+//    public Validator getValidator() {
+//        return new ItemValidator();
+//    }
 
 }
